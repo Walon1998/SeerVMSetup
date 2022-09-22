3084fd2e9cb78948d657a05d87f75f8a8b860b02
Neville Walo <neville.walo@gmail.com>
2022-09-22 22:08:34 +0200

    fix

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco --version
-choco install -y python --version = 3.9.13
+choco install -y python --version=3.9.13
 choco install git
 refreshenv
 pip install --upgrade pip
