689205faaaef1c377e928d4da8f4d42250edb121
Neville Walo <neville.walo@gmail.com>
2022-09-22 22:51:59 +0200

    fix

--- install_1.ps1
+++ install_1.ps1
@@ -1,7 +1,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install_1.ps1'))
 choco --version
 choco install -y python --version=3.9.13
-choco install git
+choco install -y git
+refreshenv
 
 
 
--- install_2.ps1
+++ install_2.ps1
@@ -1,4 +1,3 @@
-refreshenv
 pip install --upgrade pip
 pip install torch --extra-index-url https://download.pytorch.org/whl/cu116
 pip install legendary-gl
@@ -10,7 +9,7 @@ New-Item -ItemType File -Path $file -Force
 Copy-Item "./TASystemSettings.ini" -Destination $file -Force
 pip install git+https://github.com/Walon1998/SeerPPO
 git clone https://github.com/Walon1998/SeerWorker
-cd cd SeerWorker
+cd SeerWorker
 wget http://nevillewalo.ch/assets/Replays.zip
 Expand-Archive -DestinationPath ".\Replays" .\Replays.zip
 
