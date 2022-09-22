0a2c1cf4d584c2e1c5d1df3de038b04c4cd161ce
Neville Walo <neville.walo@gmail.com>
2022-09-22 21:47:32 +0200

    Initial commit

---
+++ TODO.md
@@ -0,0 +1,4 @@
+1. Open PowerShell in Admin Mode: `Set-ExecutionPolicy RemoteSigned`
+2. Run install script: `./install.ps1`
+3. Download and run bakkesmod
+4. Start
---
+++ install.ps1
@@ -0,0 +1,24 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco --version
+choco install -y python --version = 3.9.13
+pip install --upgrade pip
+pip install torch --extra-index-url https://download.pytorch.org/whl/cu116
+Copy-Item "./TASystemSettings.ini" -Destination "C:/Users/neville/Documents/My Games/Rocket League/TAGame/Config"
+choco install git
+refreshenv
+pip install legendary-gl
+refreshenv
+legendary auth
+"Y" | legendary install Rocket League
+pip install git+https://github.com/Walon1998/SeerPPO
+git clone https://github.com/Walon1998/SeerWorker
+cd cd SeerWorker
+wget http://nevillewalo.ch/assets/Replays.zip
+Expand-Archive -DestinationPath ".\Replays" .\Replays.zip
+
+
+
+
+
+
+
