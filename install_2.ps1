90de21930d40b581978aa7eac2f5e3b76afce0b5
Neville Walo <neville.walo@gmail.com>
2022-09-23 14:32:44 +0200

    update

--- install_2.ps1
+++ install_2.ps1
@@ -7,9 +7,10 @@ legendary auth
 $file = "C:/Users/neville/Documents/My Games/Rocket League/TAGame/Config/TASystemSettings.ini"
 New-Item -ItemType File -Path $file -Force
 Copy-Item "./TASystemSettings.ini" -Destination $file -Force
-pip install git+https://github.com/Walon1998/SeerPPO
 git clone https://github.com/Walon1998/SeerWorker
 cd SeerWorker
+pip install -r requirements.txt
+pip install -r requirements.txt --upgrade
 cd Replays
 $files = "Replays_0.zip", "Replays_1.zip", "Replays_2.zip", "Replays_3.zip", "Replays_4.zip"
 foreach ($f in $files)
